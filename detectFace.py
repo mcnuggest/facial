9349324d1fdb2ca6cf60cf49485d1cd138309c3a
mcnuggest <545467834@qq.com>
2017-12-06 15:33:53 +0800

    加入403错误的解决

--- detectFace.py
+++ detectFace.py
@@ -12,22 +12,30 @@ __face1 = open(r"face1.jpg", 'rb')
 __ls_f = base64.b64encode(__face1.read())
 __face1.close()
 
-__textmod = {"api_key": __apiKey, "api_secret": __apiSecret, "image_base64": __ls_f,"return_attributes":"gender,smiling,beauty"}
+__textmod = {"api_key": __apiKey, "api_secret": __apiSecret, "image_base64": __ls_f,
+             "return_attributes": "gender,smiling,beauty"}
 __textmod = parse.urlencode(__textmod).encode(encoding='utf-8')
 
-__req = request.Request(url=__detectApi, data=__textmod)
-__res = request.urlopen(__req)
-__res = __res.read()
-__res=__res.decode(encoding='utf-8')
-print(__res)
+try:
+    __req = request.Request(url=__detectApi, data=__textmod)
+    __res = request.urlopen(__req)
+    __res = __res.read()
+    __res = __res.decode(encoding='utf-8')
+    print(__res)
+    __resJson = json.loads(__res)
+    if (__resJson['faces']):
+        __face_token = __resJson['faces'][0]['face_token']
+        print(__face_token)
+    else:
+        __face_token = "don't have face"
+        print(__face_token)
+
+except:
+    print("检测人脸失败，重新尝试")
+    __face_token = "failed"
+    print(__face_token)
 # res='{"image_id": "t+JQq9FEcwTH0fc4cCEbYA==", "request_id": "1512542943,31634c89-1da2-4b8a-b70c-343249b42e44", "time_used": 197, "faces": []}'
 
-__resJson = json.loads(__res)
-if(__resJson['faces']):
-    __face_token = __resJson['faces'][0]['face_token']
-    print(__face_token)
-else:
-    print("don't have face")
 
 
 def returnFaceToken():
