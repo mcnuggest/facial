962cc55a4295dd87189fbb3dc7bc4a4d319d9466
mcnuggest <545467834@qq.com>
2017-12-10 21:46:37 +0800

    finish code stage1

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import detectFace
 import compareFaces
 
 a = detectFace.returnFaceToken("face1.jpg")
-print(a)
+if a=="don't have face":
+    print(a)
+elif a=="检测人脸失败，重新尝试":
+    print(a)
+else:
+    print(a)
+    print("是否保存")
 
-b = detectFace.returnFaceToken("face1.jpg")
+
+
+
+b = detectFace.returnFaceToken("face4.jpg")
 print(b)
 
 compareFaces.compareFace(a,b)
