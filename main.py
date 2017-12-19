8242d45ffb9fcf96b8cfdbffd3c526f82f625447
mcnuggest <545467834@qq.com>
2017-12-19 14:14:54 +0800

    add gui

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import detectFace
 import compareFaces
+import gui
 
-a = detectFace.returnFaceToken("face1.jpg")
-if a=="don't have face":
-    print(a)
-elif a=="检测人脸失败，重新尝试":
-    print(a)
-else:
-    print(a)
-    print("是否保存")
+# gui()
 
 
 
-
-b = detectFace.returnFaceToken("face4.jpg")
-print(b)
-
-compareFaces.compareFace(a,b)
+# a = detectFace.returnFaceToken("face1.jpg")
+# if a=="don't have face":
+#     print(a)
+# elif a=="检测人脸失败，重新尝试":
+#     print(a)
+# else:
+#     print(a)
+#     print("是否保存")
+#
+# b = detectFace.returnFaceToken("face4.jpg")
+# print(b)
+#
+# compareFaces.compareFace(a,b)
 
